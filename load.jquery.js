7c7b91a788433de6f5e39c62dfc5a898a8014497
Ivijan-Stefan Stipic <creativform@gmail.com>
2018-03-13 13:52:55 +0100

    Update load.jquery.js

--- load.jquery.js
+++ load.jquery.js
@@ -1,33 +1,37 @@
 /**
-	 * Define jQuery if is not already defined
-	 *
-	 * @autor    Ivijan-Sefan Stipic
-	 * @version  1.0.0
-	**/
-	(function(url, position, callback){
-		// Check is jQuery exists
-		if (!window.jQuery) {
-			// Initialize <head>
-			var head = document.getElementsByTagName('head')[0];
-			// Create <script> element
-			var script = document.createElement("script");
-			// Append URL
-			script.src = url;
-			// Append type
-			script.type = 'text/javascript';
-			// Append script to <head>
-			head.appendChild(script);
-			// Move script on proper position
-			head.insertBefore(script,head.childNodes[position]);
+ * Define jQuery if is not already defined
+ *
+ * @autor    Ivijan-Sefan Stipic
+ * @version  1.0.0
+**/
+(function(url, position, callback){
+	// default values
+	url = url || 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+	position = position || 0;
 
-			script.onload = function(){
-				if(typeof callback == 'function') {
-					callback(jQuery);
-				}
-			};
-		} else {
+	// Check is jQuery exists
+	if (!window.jQuery) {
+		// Initialize <head>
+		var head = document.getElementsByTagName('head')[0];
+		// Create <script> element
+		var script = document.createElement("script");
+		// Append URL
+		script.src = url;
+		// Append type
+		script.type = 'text/javascript';
+		// Append script to <head>
+		head.appendChild(script);
+		// Move script on proper position
+		head.insertBefore(script,head.childNodes[position]);
+
+		script.onload = function(){
 			if(typeof callback == 'function') {
 				callback(jQuery);
 			}
+		};
+	} else {
+		if(typeof callback == 'function') {
+			callback(jQuery);
 		}
-	}('[CUSTOM URL]', 0));
+	}
+}();
